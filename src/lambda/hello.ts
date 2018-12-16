a009232da153d890267e770e4aa01ad43ddece82
alexdriaguine <alexdriagin12@gmail.com>
2018-12-16 01:07:36 +0100

    mongodb.

--- src/lambda/hello.ts
+++
@@ -1,8 +0,0 @@
-import { Handler } from "aws-lambda";
-
-export const handler: Handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello, world!',
-  });
-};
